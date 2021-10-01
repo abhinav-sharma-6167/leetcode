b1d089ae3456f69e8c9eb4bf5843e9669220ac2e
Abhinav Sharma <abhinavs01858@gmail.com>
2021-10-01 16:49:11 -0500

    Usage of itertools product(*iterable)

---
+++ hackerrank/maximize_it.py
@@ -0,0 +1,16 @@
+from itertools import product
+K, M = map(int,input().split())
+def Sq(n):
+    return int(n)**2
+List = []
+for i in range(K):
+    List.append(list(map(Sq,input().split()[1:])))
+Max=0  
+#print(List)
+#print(product(*List))  
+#for T in product(*List):
+    print(T)
+    Sum=sum(T)%M
+    if Sum>Max:   #finding maximum sum out of all tuples
+        Max=Sum
+print(Max)
